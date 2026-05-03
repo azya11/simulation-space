aff31fc0c8211572652318c05e8cd61405704137
azya11 <ashamura@asu.edu>
2026-05-03 04:19:24 -0700

    start

---
+++ README.md
@@ -0,0 +1,3 @@
+###THIS PROJECT HAS 0 AI CODE INSIDE###
+## Library used for 3D: https://www.ogre3d.org/download/sdk/sdk-ogre 
+https://learn.microsoft.com/en-us/cpp/porting/binary-compat-2015-2017?view=msvc-160
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+
+using namespace std;
+
+class Object {
+    public:
+        long long mass;
+        float position[x][y];
+        float velocity[x][y];
+}
+
+int main() {
+    int i = 0;
+
+    Object earth;
+    earth.mass = 5.9722*pow(10,24); //in kilograms
+    earth.position = [149597870700][0]; // in meters
+    earth.velocity = [29783][0]; // in m/s
+
+    Object sun;
+    sun.mass = 1.9885*pow(10,30);
+    sun.position = [0][0]; 
+    sun.velocity = [0][0];
+
+    while(i=0){
+        
+
+    }
+
+}
+
